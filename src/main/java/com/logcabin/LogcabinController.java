bfeca1f589f765c5883dccae414cb55b43a050ff
Administrator <Administrator@DESKTOP-099UQRG>
2020-10-25 12:26:28 -0400

    Comments explaining controller

---
+++ src/main/java/com/logcabin/LogcabinController.java
@@ -0,0 +1,60 @@
+package com.logcabin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.logcabin.dto.ApplicationDTO;
+import com.logcabin.services.IApplicationService;
+
+
+@Controller
+public class LogcabinController {
+	
+	// There should be some comments explaining what this file does
+	
+	@Autowired
+	private IApplicationService applicationServiceStub;
+	
+	@RequestMapping(value="/home", params= {"application=facebook"})
+	public String facebook(Model model){
+		
+		ApplicationDTO applicationDTO = applicationServiceStub.fetchByName("Facebook");
+		model.addAttribute("applicationDTO", applicationDTO);
+		return "application";
+
+	}
+	
+	@RequestMapping(value="/home", params= {"application=instagram"})
+	public String instagram(Model model){
+		
+		ApplicationDTO applicationDTO = applicationServiceStub.fetchByName("Instagram");
+		model.addAttribute("applicationDTO", applicationDTO);
+		return "application";
+
+	}
+	
+	@RequestMapping(value="/home", params= {"application=twitter"})
+	public String twitter(Model model){
+		
+		ApplicationDTO applicationDTO = applicationServiceStub.fetchByName("Twitter");
+		model.addAttribute("applicationDTO", applicationDTO);
+		return "application";
+
+	}
+	
+	@RequestMapping(value="/home")
+	public String home(){
+		
+		return "home";
+
+	}
+	
+	@RequestMapping("/")
+	public String Index(){
+		
+		return "home";
+	}
+}
