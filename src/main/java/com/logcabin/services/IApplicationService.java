2aa9333d6ffe094b7c6c501961553fc8ff96d9d7
alongemo <71347001+alongemo@users.noreply.github.com>
2020-11-22 20:51:09 -0500

    Update IApplicationService.java
    Added imports for requestmapping and requestmethod

--- src/main/java/com/logcabin/services/IApplicationService.java
+++ src/main/java/com/logcabin/services/IApplicationService.java
@@ -1,7 +1,8 @@
 package com.logcabin.services;
 
 import com.logcabin.dto.ApplicationDTO;
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 public interface IApplicationService {
 
 	ApplicationDTO fetchByName(String name);
